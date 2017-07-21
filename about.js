bb8c76e2105412d4ee2363b58de72751fd7ae264
moekengara02 <2020kengaramaureenw@aaps.k12.mi.us>
2017-07-21 11:17:32 -0400

    Update about.js

--- about.js
+++ about.js
@@ -1,12 +1,12 @@
 $("#guess_button").click( function() {
     var input =$("#inputbox").val();
-    input.setAttribute("type", "number");
+    var value = +(input.value) || 0;
     var fav_number= "2";
     
-    if(input < fav_number){
+    if(value < fav_number){
         $("p").html("Too low!");
     }
-    else if(input > fav_number){
+    else if(value > fav_number){
         $("p").html("Too high!");
     }
     else{
